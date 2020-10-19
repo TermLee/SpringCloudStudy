3d3ca58641c04715b28c2308861336765ccee893
Termlis <v-teli@expedia.com>
2020-10-19 17:46:14 +0800

    add SQL file

---
+++ SQL/db02.sql
@@ -0,0 +1,43 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50648
+Source Host           : 127.0.0.1:3306
+Source Database       : db02
+
+Target Server Type    : MYSQL
+Target Server Version : 50648
+File Encoding         : 65001
+
+Date: 2020-10-19 17:43:49
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(255) DEFAULT NULL,
+  `db_source` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`deptno`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES ('1', '开发部', 'db02');
+INSERT INTO `dept` VALUES ('2', '测试部', 'db02');
+INSERT INTO `dept` VALUES ('3', '产品部', 'db02');
+INSERT INTO `dept` VALUES ('4', '运维部', 'db02');
+INSERT INTO `dept` VALUES ('5', null, 'db02');
+INSERT INTO `dept` VALUES ('6', '查业务部', 'db02');
+INSERT INTO `dept` VALUES ('7', '财务部', 'db02');
+INSERT INTO `dept` VALUES ('8', '财务部', 'db02');
+INSERT INTO `dept` VALUES ('9', '秘书科', 'db02');
+INSERT INTO `dept` VALUES ('10', '秘书科', 'db02');
+INSERT INTO `dept` VALUES ('11', '保安科', 'db02');
+INSERT INTO `dept` VALUES ('12', '主席团', 'db02');
